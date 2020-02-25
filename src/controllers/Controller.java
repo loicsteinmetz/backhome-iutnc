84c244a29dcda4fbfaf0b8cf85c5693d9db3447b
Loïc Steinmetz <loic.steinmetz2@gmail.com>
2020-02-25 21:46:33 +0100

    front accueil du jeu

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,4 +1,33 @@
 package controllers;
 
+import javafx.animation.Interpolator;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
 public class Controller {
+
+    @FXML
+    private ImageView starsBg1,starsBg2;
+
+    public void animateBg() {
+        TranslateTransition tt1 = new TranslateTransition(Duration.seconds(1.5), starsBg1);
+        tt1.setFromY(-600);
+        tt1.setToY(0);
+        tt1.setCycleCount( Timeline.INDEFINITE);
+        tt1.setInterpolator(Interpolator.LINEAR);
+        tt1.play();
+        TranslateTransition tt2 = new TranslateTransition(Duration.seconds(1.5), starsBg2);
+        tt2.setFromY(0);
+        tt2.setToY(600);
+        tt2.setCycleCount(Timeline.INDEFINITE);
+        tt2.setInterpolator(Interpolator.LINEAR);
+        tt2.play();
+    }
+
+    public void initialize(){
+        animateBg();
+    }
 }
--- src/models/BackHome.java
+++ src/models/BackHome.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class BackHome extends Application {
@@ -11,8 +12,12 @@ public class BackHome extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../views/BackHome.fxml"));
+        Scene scene = new Scene(root, 800, 600);
+        Font.loadFont(BackHome.class.getResource("/assets/fonts/ZCOOLQingKeHuangYou-Regular.ttf").toExternalForm(), 10);
+        Font.loadFont(BackHome.class.getResource("/assets/fonts/RobotoSlab-VariableFont:wght.ttf").toExternalForm(), 10);
+        scene.getStylesheets().add("assets/css/BackHome.css");
         primaryStage.setTitle("Back Home");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
