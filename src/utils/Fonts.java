1fca034c6dd913ed5907d871065554ef7b33382c
Loïc Steinmetz <loic.steinmetz2@gmail.com>
2020-02-26 10:25:16 +0100

    doc utils.fonts

--- src/utils/Fonts.java
+++ src/utils/Fonts.java
@@ -3,8 +3,14 @@ package utils;
 import javafx.scene.text.Font;
 import models.BackHome;
 
+/**
+ * Classe utilitaire de gestion des polices d'écriture
+ */
 public class Fonts {
 
+    /**
+     * Charge les polices
+     */
     public static void load(){
         Font.loadFont(BackHome .class.getResource("/assets/fonts/ZCOOLQingKeHuangYou-Regular.ttf").toExternalForm(), 10);
         Font.loadFont(BackHome.class.getResource("/assets/fonts/RobotoSlab-VariableFont:wght.ttf").toExternalForm(), 10);
