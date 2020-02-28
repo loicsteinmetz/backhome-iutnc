3cb262b26c40c2ac437bd4c803afd3b256f625d5
unknown <quoc-hieu.phu1@etu.univ-lorraine.fr>
2020-02-28 15:16:21 +0100

    adaptation fonts + java8

--- src/utils/Fonts.java
+++ src/utils/Fonts.java
@@ -14,15 +14,9 @@ public class Fonts {
      * Charge les polices
      */
     public static void load(){
-        Font.loadFont(BackHome .class.getResource(
-          File.separator + "assets" +
-                File.separator + "fonts" +
-                File.separator + "ZCOOLQingKeHuang.ttf")
+        Font.loadFont(BackHome .class.getResource("/assets/fonts/ZCOOLQingKeHuang.ttf")
             .toExternalForm(), 10);
-        Font.loadFont(BackHome.class.getResource(
-          File.separator + "assets" +
-                File.separator + "fonts" +
-                File.separator + "RobotoSlab.ttf")
+        Font.loadFont(BackHome.class.getResource("/assets/fonts/RobotoSlab.ttf")
             .toExternalForm(), 10);
     }
 }
