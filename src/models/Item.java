d828e95d0c840e83aad17d2bd515773e8b579b0f
Louis Friedrich <louisf.28@hotmail.fr>
2020-03-07 08:16:54 +0100

    corrige Item

--- src/models/Item.java
+++ src/models/Item.java
@@ -1,17 +1,21 @@
 package models;
 
-public class Item {
+public abstract class Item {
 
     protected String nom;
     protected int id;
 
+    /**
+     * Constructeur
+     * @param id l'id de l'item
+     */
     public Item(int id){
         this.id = id;
     }
 
     /**
      * Getter
-     * @return nom de l'armure
+     * @return nom de l'item
      */
     public String getNom()
     {
