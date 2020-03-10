abf17b68dde7e751e3c53b74bca8db6734041dc7
Louis Friedrich <louisf.28@hotmail.fr>
2020-03-10 15:44:42 +0100

    implémente chargeSauvegarde

--- src/models/Sauvegarde.java
+++ src/models/Sauvegarde.java
@@ -1,6 +1,30 @@
 package models;
 
+import lib.org.json.simple.JSONObject;
+import lib.org.json.simple.parser.ParseException;
+import utils.JsonParser;
+
+import java.io.IOException;
+
+import static models.Inventaire.getInventaire;
+
 public class Sauvegarde {
 
-    
+    public static void chargeSauvegarde(int id){
+        String chemin = "/data/sauvegardes.json";
+        String cle = Integer.toString(id);
+        JSONObject save = null;
+        try {
+            save = new JsonParser().parseObject(chemin,cle);
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        if (save != null){
+            getInventaire().setArmeCac(new ArmeCac((int)(long)save.get("idArmeCac")));
+            getInventaire().setArmeDist(new ArmeDistance((int)(long)save.get("idArmeDistance")));
+            getInventaire().setArmure(new Armure((int)(long)save.get("idArmure")));
+            getInventaire().modifierCarburant((int)(long)save.get("carbu"));
+
+        }
+    }
 }
