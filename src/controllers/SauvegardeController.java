f4e1ed3a9a04fb4ae25a9d0ac99e48c254057b37
Loïc Steinmetz <loic.steinmetz2@gmail.com>
2020-03-11 09:50:49 +0100

    crée feuille de style et controleur sauvegarde

--- src/controllers/SauvegardeController.java
+++ src/controllers/SauvegardeController.java
@@ -1,4 +1,40 @@
 package controllers;
 
-public class SauvegardeController {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import models.Sauvegarde;
+
+public class SauvegardeController extends Application {
+
+    @Controller
+    private static final String VIEW = "/views/Sauvegarde.fxml";
+    @Controller
+    private static final String STYLE = "/assets/css/Sauvegarde.css";
+    @Controller
+    private static Sauvegarde MODELE;
+
+    /**
+     * Retourne la vue associée au controller
+     * @return chemin de la vue
+     */
+    @Controller
+    public static String getView(){
+        return VIEW;
+    }
+
+    /**
+     * Génère l'interface propres aux sauvegardes
+     * @param stage primaryStage
+     * @throws Exception chargement de la vue
+     */
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource(VIEW));
+        Scene scene = new Scene(root, 800, 600);
+        scene.getStylesheets().add(STYLE);
+        stage.setScene(scene);
+    }
 }
