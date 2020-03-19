68d96e6dd97532d73e1235123c4086505bb139dd
Louis Friedrich <louisf.28@hotmail.fr>
2020-03-19 12:47:03 +0100

    revoit construcs items

--- src/tests/TestCombat.java
+++ src/tests/TestCombat.java
@@ -2,4 +2,12 @@ package tests;
 
 public class TestCombat {
 
+    public void testFaiblesseTireur(){
+        //Tireur t = new Tireur(100, new ArmeDistance(0,))
+    }
+
+    public static void main(String[] args) {
+        //lancer(new TestCombat(),args);
+    }
 }
+
