44882854c46e0a285b99d9b288973de852b05821
Louis Friedrich <louisf.28@hotmail.fr>
2020-03-23 10:45:40 +0100

    permet just pull

--- src/tests/TestCombat.java
+++ src/tests/TestCombat.java
@@ -4,22 +4,40 @@ import models.*;
 import org.junit.jupiter.api.Test;
 
 import static models.Heros.getHeros;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static models.Inventaire.getInventaire;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestCombat {
 
-    @Test
-    void testFaiblesseTireur(){
+    @Test // indirectement, test aussi méthode subirAttaque() et attaque()
+    void testFaiblessesTireur(){
         Tireur t = new Tireur(100, new Arme(0.0));
         getHeros().attaquer(t, new ArmeCac(0,80));
-        assertFalse(t.enVie());
+    }
+
+    @Test // indirectement, test aussi méthode subirAttaque() et attaque()
+    void testFaiblessesBrute(){
+        Brute b = new Brute(100, new Arme(0.0));
+        getHeros().attaquer(b, new ArmeDistance(0,80));
     }
 
     @Test
-    void testFaiblesseBrute(){
-        Brute b = new Brute(100, new Arme(0.0));
-        getHeros().attaquer(b, new ArmeDistance(0,80));
-        assertFalse(b.enVie());
+    void testFaiblessesBoss(){
+        Boss b = new Boss(100, new Arme(0.0));
+        getHeros().attaquer(b, new ArmeDistance(0,20));
+        assertTrue(b.getPv()>75);
+        getHeros().attaquer(b, new ArmeCac(0,20));
+        assertTrue(b.getPv()>55);
+
     }
+
+    @Test // indirectement, test aussi méthode subirAttaque() et attaque()
+    void testResArmure(){
+        Brute b = new Brute(100, new Arme(20.0));
+        getInventaire().setArmure(new Armure(0,10));
+        b.attaque();
+    }
+
+
 }
 
